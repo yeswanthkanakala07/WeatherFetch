e16def868b0c2ae60366464aad00d4bc9538cc51
YASWANTH KANAKALA <82636668+yeswanthkanakala07@users.noreply.github.com>
2025-01-15 17:49:31 -0800

    Created Spring Boot Application

---
+++ src/main/java/Controllers/HelloWorld.java
@@ -0,0 +1,8 @@
+package Controllers;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+@Controller
+public class HelloWorld {
+
+}
---
+++ src/main/java/org/example/dataaccessapi/DataAccessApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.dataaccessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAccessApiApplication.class, args);
+    }
+
+}
