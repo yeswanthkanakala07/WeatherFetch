f763fc0eb00831bbe0efea7da8cf835b39f90206
YASWANTH KANAKALA <82636668+yeswanthkanakala07@users.noreply.github.com>
2025-01-15 17:49:31 -0800

    Created Spring Boot Application

---
+++ src/test/java/org/example/dataaccessapi/DataAccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dataaccessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
